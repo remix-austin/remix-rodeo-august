41f1242642222b3c7a073dadcbb8e8d2e2637fea
Seth Davis <sethdavis512@gmail.com>
2024-07-26 16:54:27 -0500

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const baseUrl = '/remix-rodeo-august/';
 
 export default defineConfig({
-  plugins: [
-    remix({
-      ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+    base: baseUrl,
+    plugins: [
+        remix({
+            basename: baseUrl,
+            ssr: false,
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true
+            }
+        }),
+        tsconfigPaths()
+    ]
 });
