66b6050b6b7b3684bbe9d57635e5f43490a408c2
Seth Davis <sethdavis512@gmail.com>
2024-08-07 12:11:56 -0500

    Clean up render

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import type { MetaFunction } from '@remix-run/node';
+import { json, type MetaFunction } from '@remix-run/node';
 import * as Tabs from '@radix-ui/react-tabs';
+import { useLoaderData } from '@remix-run/react';
 
 import Heading from '~/components/Heading';
 import LinkList from '~/components/LinkList';
 import TabContent from '~/components/TabContent';
 import TabTrigger from '~/components/TabTrigger';
+import allThemLinks from '~/utils/data.json';
 
 export const meta: MetaFunction = () => {
     return [
@@ -14,7 +16,12 @@ export const meta: MetaFunction = () => {
     ];
 };
 
+export async function loader() {
+    return json(allThemLinks);
+}
+
 export default function Index() {
+    const data = useLoaderData<typeof loader>();
     const [activeTab, setActiveTab] = useState('stacksTab');
 
     return (
@@ -29,36 +36,7 @@ export default function Index() {
                     <p>{`Build a game that uses Remix's key features:`}</p>
                     <LinkList
                         className="mt-2 mb-4"
-                        items={[
-                            {
-                                href: 'https://remix.run/docs/en/main/route/loader',
-                                label: 'Loaders'
-                            },
-                            {
-                                href: 'https://remix.run/docs/en/main/route/action',
-                                label: 'Actions'
-                            },
-                            {
-                                href: 'https://remix.run/docs/en/main/route/form',
-                                label: 'Form'
-                            },
-                            {
-                                href: 'https://remix.run/docs/en/main/hooks/use-fetcher',
-                                label: 'Fetchers'
-                            },
-                            {
-                                href: 'https://remix.run/docs/en/main/file-conventions/routes',
-                                label: 'Nested routes'
-                            },
-                            {
-                                href: 'https://remix.run/docs/en/main/discussion/progressive-enhancement',
-                                label: 'Progressive enhancement'
-                            },
-                            {
-                                href: 'https://remix.run/docs/en/main/discussion/pending-ui',
-                                label: 'Optimistic UI'
-                            }
-                        ]}
+                        items={data.remixFeaturesLinks}
                     />
                     <p>{`Try storing the player's score. Post everyones game stats. Create a gamer profile. Store it all in a database. Make a live interactive game. Build as much as you can as fast as you can.`}</p>
                 </div>
@@ -87,34 +65,7 @@ export default function Index() {
                         </li>
                     </ul>
                     <Heading>Real world examples (non-Remix)</Heading>
-                    <LinkList
-                        items={[
-                            {
-                                label: 'Cookie Clicker',
-                                href: 'https://cookieclicker.ee/'
-                            },
-                            {
-                                label: 'OGame',
-                                href: 'https://en.wikipedia.org/wiki/OGame'
-                            },
-                            {
-                                label: 'Puzzles',
-                                href: 'https://puzzlegarage.com/'
-                            },
-                            {
-                                label: 'Wordle',
-                                href: 'https://www.nytimes.com/games/wordle/index.html'
-                            },
-                            {
-                                label: 'Escape room',
-                                href: 'https://cuadventures.com/at-home'
-                            },
-                            {
-                                label: 'Quizzes (Buzzfeed)',
-                                href: 'https://www.buzzfeed.com/quizzes'
-                            }
-                        ]}
-                    />
+                    <LinkList items={data.realWorldExamplesLinks} />
                 </div>
             </div>
             <Heading>Resources</Heading>
@@ -163,76 +114,23 @@ export default function Index() {
                         Extras
                     </TabTrigger>
                 </Tabs.List>
-                <TabContent value="remixTab">
-                    <LinkList
-                        items={[
-                            {
-                                label: 'Remix Docs',
-                                href: 'https://remix.run/docs'
-                            },
-                            {
-                                label: 'Remix Youtube Playlists',
-                                href: 'https://www.youtube.com/@Remix-Run/playlists'
-                            },
-                            {
-                                label: 'SPA Mode Guide',
-                                href: 'https://remix.run/future/spa-mode'
-                            }
-                        ]}
-                    />
-                </TabContent>
-                <TabContent value="librariesTab">
-                    <Heading>Gaming</Heading>
-                    <LinkList
-                        items={[
-                            {
-                                label: 'Pixi JS',
-                                href: 'https://github.com/pixijs/pixijs'
-                            },
-                            {
-                                label: 'Phaser',
-                                href: 'https://github.com/phaserjs/phaser'
-                            },
-                            {
-                                label: 'Kaboom',
-                                href: 'https://github.com/replit/kaboom'
-                            },
-                            {
-                                label: 'More game engines...',
-                                href: 'https://github.com/collections/javascript-game-engines'
-                            }
-                        ]}
-                    />
-                </TabContent>
                 <TabContent value="stacksTab">
                     <p className="mb-2">
                         {`Wanna use Tailwind? It's baked into the default template
-                    when you run `}
+                                when you run `}
                         <code className="bg-gray-600 text-white py-1 px-2 rounded-lg text-sm">
                             npx create-remix@latest
                         </code>{' '}
                         or use a prebuilt stack:
                     </p>
-                    <LinkList
-                        items={[
-                            {
-                                label: 'Indie stack',
-                                href: 'https://github.com/remix-run/indie-stack'
-                            },
-                            {
-                                label: 'Blues stack',
-                                href: 'https://github.com/remix-run/blues-stack'
-                            },
-                            {
-                                label: 'PartyKit starter',
-                                href: 'https://github.com/partykit/remix-starter'
-                            },
-                            {
-                                label: 'More stacks (GitHub)...',
-                                href: 'https://github.com/topics/remix-stack'
-                            }
-                        ]}
-                    />
+                    <LinkList items={data.stacksLinks} />
+                </TabContent>
+                <TabContent value="remixTab">
+                    <LinkList items={data.remixLinks} />
+                </TabContent>
+                <TabContent value="librariesTab">
+                    <Heading>Gaming</Heading>
+                    <LinkList items={data.gameLibraryLinks} />
                 </TabContent>
                 <TabContent value="databasesTab">
                     <div className="grid grid-cols-12">
@@ -240,27 +138,7 @@ export default function Index() {
                             <p className="mb-2">
                                 <strong>{`ORMs`}</strong>
                             </p>
-                            <LinkList
-                                className="mb-4"
-                                items={[
-                                    {
-                                        label: 'Prisma',
-                                        href: 'https://github.com/topics/remix-stack'
-                                    },
-                                    {
-                                        label: 'Drizzle',
-                                        href: 'https://orm.drizzle.team/docs/overview'
-                                    },
-                                    {
-                                        label: 'Sequelize',
-                                        href: 'https://sequelize.org/'
-                                    },
-                                    {
-                                        label: 'TypeORM',
-                                        href: 'https://typeorm.io/'
-                                    }
-                                ]}
-                            />
+                            <LinkList className="mb-4" items={data.ORMLinks} />
                         </div>
                         <div className="col-span-2">
                             <p className="mb-2">
@@ -268,24 +146,7 @@ export default function Index() {
                             </p>
                             <LinkList
                                 className="mb-4"
-                                items={[
-                                    {
-                                        label: 'Fly.io',
-                                        href: 'https://fly.io/docs/'
-                                    },
-                                    {
-                                        label: 'Render',
-                                        href: 'https://render.com/'
-                                    },
-                                    {
-                                        label: 'Koyeb',
-                                        href: 'https://www.koyeb.com/'
-                                    },
-                                    {
-                                        label: 'Heroku',
-                                        href: 'https://www.heroku.com/'
-                                    }
-                                ]}
+                                items={data.hostsLinks}
                             />
                         </div>
                         <div className="col-span-2">
@@ -294,49 +155,13 @@ export default function Index() {
                             </p>
                             <LinkList
                                 className="mb-4"
-                                items={[
-                                    {
-                                        label: 'Neon DB',
-                                        href: 'https://neon.tech/'
-                                    },
-                                    {
-                                        label: 'Supabase',
-                                        href: 'https://supabase.com/'
-                                    }
-                                ]}
+                                items={data.servicesLinks}
                             />
                         </div>
                     </div>
                 </TabContent>
                 <TabContent value="tutorialsTab">
-                    <LinkList
-                        items={[
-                            {
-                                label: 'Relational databases (Prisma docs)',
-                                href: 'https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases-typescript-postgresql'
-                            },
-                            {
-                                label: 'Build A Fullstack App with Remix, Prisma & MongoDB (use first half as foundation for game)',
-                                href: 'https://www.prisma.io/blog/fullstack-remix-prisma-mongodb-1-7D0BfTXBmB6r'
-                            },
-                            {
-                                label: 'Remix on Netlify',
-                                href: 'https://docs.netlify.com/frameworks/remix/'
-                            },
-                            {
-                                label: 'Connect a Remix application to Neon',
-                                href: 'https://neon.tech/docs/guides/remix'
-                            },
-                            {
-                                label: 'Cloudflare & Remix',
-                                href: 'https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/'
-                            },
-                            {
-                                label: 'Deploy a Remix App (Render)',
-                                href: 'https://docs.render.com/deploy-remix'
-                            }
-                        ]}
-                    />
+                    <LinkList items={data.tutorialsLinks} />
                 </TabContent>
                 <TabContent value="extrasTab">
                     <iframe
